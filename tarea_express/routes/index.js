9b19cc70cfbd9c15888875ea1731a8bcdd10e28e
Valentina Grioli <vgrioli@gmail.com>
2025-02-24 23:19:21 -0300

    M3U1_Actividades

--- tarea_express/routes/index.js
+++ tarea_express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hola soy Valentina Grioli' });
+  res.render('index', { title: 'Hola soy Valentina' });
 });
 
 module.exports = router;
